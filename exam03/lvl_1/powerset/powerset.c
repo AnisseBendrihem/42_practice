db1ce1f7c67710146cd6ca6fc26bc9ad52fae2d8
abendrih <anisse.bendrihem@gmail.com>
2025-10-14 16:24:08 +0200

    fix(exam03): correct powerset implementation

--- exam03/lvl_1/powerset/powerset.c
+++ exam03/lvl_1/powerset/powerset.c
@@ -6,83 +6,58 @@
 /*   By: baron_kakka <baron_kakka@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 17:52:41 by baron_kakka       #+#    #+#             */
-/*   Updated: 2025/10/13 17:53:21 by baron_kakka      ###   ########.fr       */
+/*   Updated: 2025/10/14 16:22:51 by baron_kakka      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int	ssum(int *subset)
+int found = 0;
+
+void power( int target, int *set, int *subset, int len_set, int idx,  int len_subset, int sum)
 {
-	int	i;
-	int	sum;
-
-	i = 0;
-	sum = 0;
-	while (subset[i])
-	{
-		sum += subset[i];
-		i++;
-	}
-	return (sum);
+    if (sum == target && len_subset > 0)
+    {
+        found = 1;
+        for (int i = 0; i < len_subset; i++)
+        {
+            if (i > 0)
+                printf(" ");
+            printf("%d", subset[i]);
+        }
+        printf("\n");
+    }
+    while  (idx < len_set)
+    {
+        subset[len_subset] = set[idx];
+        power(target,set, subset, len_set,  idx + 1, len_subset + 1, sum + set[idx]);
+        idx++;
+    }
 }
 
-void	powerset(int *set, int *subset, int target, int index, int nbr_val,
-		int len_subset)
+int main(int ac, char **av)
 {
-	int	i;
-	int	sum;
-
-	i = 0;
-	sum = ssum(subset);
-	if (sum == target)
-	{
-		i = 0;
-		while (i < len_subset)
-		{
-			printf("%d ", subset[i]);
-			i++;
-		}
-		printf("\n");
-		return ;
-	}
-	
-	while (index < nbr_val)
-	{
-		if (sum + set[index] <= target)
-		{
-			subset[len_subset] = set[index];
-			powerset(set, subset, target, index + 1, nbr_val, len_subset + 1);
-			subset[len_subset] = 0;
-		}
-		index++;
-	}
-}
-
-int	main(int ac, char **av)
-{
-	int	i;
-	int	target;
-	int	nbr_val;
-	int	*set;
-	int	*subset;
-
-	i = 0;
-	if (ac <= 2)
-		return (1);
-	target = atoi(av[1]);
-	nbr_val = ac - 2;
-	set = malloc(sizeof(int) * (nbr_val));
-	subset = malloc(sizeof(int) * (nbr_val));
-	while (i < nbr_val)
-	{
-		set[i] = atoi(av[i + 2]);
-		subset[i] = 0;
-		i++;
-	}
-	powerset(set, subset, target, 0, nbr_val, 0);
-	free(set);
-	free(subset);
-	return (0);
+    if (ac < 2)
+        return 0;
+    
+    int target = atoi(av[1]);
+    int len_set = ac - 2;
+    
+    int *set = malloc(len_set * sizeof(int));
+    int *subset = malloc(len_set * sizeof(int));
+    if (!set || !subset)
+        return 1;
+    
+    for (int i = 0; i < len_set; i++)
+        set[i] = atoi(av[i + 2]);
+    
+    power(target, set, subset, len_set,  0,  0, 0);
+    
+    if (!found)
+        printf("\n");
+    
+    free(set);
+    free(subset);
+    return 0;
 }
